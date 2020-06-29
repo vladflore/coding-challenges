c2501c5748ef1d60b4f63363e811d64ff877da38
Vlad Flore <flore.vlad@gmail.com>
2020-06-29 11:39:18 +0200

    add solution for minimum distances

---
+++ hackerrank/minimum-distances/Solution.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the minimumDistances function below.
+    static int minimumDistances(int[] a) {
+        List<Integer> distances = new ArrayList<>();
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    distances.add(j - i);
+                }
+            }
+        }
+        return distances.stream().mapToInt(Integer::intValue).min().orElseGet(() -> -1);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(System.out));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int result = minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
