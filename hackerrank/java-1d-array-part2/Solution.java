7b89602c66cbe39fe54c10490cee57ac3c138118
Vlad Flore <flore.vlad@gmail.com>
2020-07-15 10:10:55 +0200

    add solution

--- hackerrank/java-1d-array-part2/Solution.java
+++ hackerrank/java-1d-array-part2/Solution.java
@@ -3,8 +3,19 @@ import java.util.*;
 public class Solution {
 
     public static boolean canWin(int leap, int[] game) {
-        // TODO not solved :(
-        return true;
+        return isWinnable(leap, game, 0);
+    }
+
+    private static boolean isWinnable(int leap, int[] game, int currentPosition) {
+        if (currentPosition < 0 || game[currentPosition] == 1)
+            return false;
+        if ((currentPosition == game.length - 1) || currentPosition + leap > game.length - 1)
+            return true;
+
+        game[currentPosition] = 1;
+
+        return isWinnable(leap, game, currentPosition + 1) || isWinnable(leap, game, currentPosition - 1)
+                || isWinnable(leap, game, currentPosition + leap);
     }
 
     public static void main(String[] args) {
