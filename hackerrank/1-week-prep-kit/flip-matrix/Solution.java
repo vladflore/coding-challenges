602a7995556597bc21ffac14190b4785cfdb7434
Vlad Flore <flore.vlad@gmail.com>
2022-01-22 23:10:55 +0100

    Start on flip matrix

---
+++ hackerrank/1-week-prep-kit/flip-matrix/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2 },
+                { 3, 4 }
+        };
+        System.out.println(new Solution().flippingMatrix(matrix));
+    }
+
+    int flippingMatrix(int[][] matrix) {
+        // TODO solve me!
+        return -1;
+    }
+}
