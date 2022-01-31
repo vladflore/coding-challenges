e5b352634174e48e234d45909ec1ee1cce825952
Vlad Flore <vlad.flore@exb.de>
2022-01-31 17:38:39 +0100

    Solve problem

---
+++ educative/MinSizeSubarrayWithGreaterSum.java
@@ -0,0 +1,26 @@
+public class MinSizeSubarrayWithGreaterSum {
+    private static int findMinSubArray(int targetSum, int[] arr) {
+
+        int winStart = 0, winSum = 0, minLen = Integer.MAX_VALUE;
+        for (int winEnd = 0; winEnd < arr.length; winEnd++) {
+            winSum += arr[winEnd];
+            while (winSum >= targetSum) {
+                minLen = Math.min(minLen, winEnd - winStart + 1);
+                winSum -= arr[winStart];
+                winStart++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+
+    public static void main(String[] args) {
+        int result = MinSizeSubarrayWithGreaterSum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
+        System.out.println("Smallest subarray length: " + result);
+        result = MinSizeSubarrayWithGreaterSum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+        System.out.println("Smallest subarray length: " + result);
+        result = MinSizeSubarrayWithGreaterSum.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+        System.out.println("Smallest subarray length: " + result);
+    }
+
+}
