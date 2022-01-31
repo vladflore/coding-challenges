13d6b6d34dba919d1d2fcecfeb0541b5c7a5dca1
Vlad Flore <flore.vlad@gmail.com>
2022-01-31 17:11:41 +0100

    Add new problems

--- educative/AverageOfSubarrayOfSizeKWithSlidingWindow.java
+++ educative/AverageOfSubarrayOfSizeKWithSlidingWindow.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
+
 class AverageOfSubarrayOfSizeKWithSlidingWindow {
-    
-    public static double[] findAverages(int k, int[] arr){
+
+    public static double[] findAverages(int k, int[] arr) {
         double[] result = new double[arr.length - k + 1];
         double windowSum = 0;
         int windowStart = 0;
-        for(int windowEnd=0;windowEnd<arr.length;windowEnd++){
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
             windowSum += arr[windowEnd];
-            if(windowEnd >= k-1){
+            if (windowEnd >= k - 1) {
                 result[windowStart] = windowSum / k;
                 windowSum -= arr[windowStart];
                 windowStart++;
@@ -17,6 +18,7 @@ class AverageOfSubarrayOfSizeKWithSlidingWindow {
     }
 
     public static void main(String args[]) {
-        System.out.println(Arrays.toString(AverageOfSubarrayOfSizeKWithSlidingWindow.findAverages(5, new int[]{1,3,2,6,-1,4,1,8,2})));
+        System.out.println(Arrays.toString(
+                AverageOfSubarrayOfSizeKWithSlidingWindow.findAverages(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 })));
     }
 }
