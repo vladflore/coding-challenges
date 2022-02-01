ffeef9c6660ed039cd9c0c5bb8c061fe7f3d9682
Vlad Flore <vlad.flore@exb.de>
2022-02-01 20:17:37 +0100

    Add new problems and restructure code

---
+++ educative/grokking_coding_interview/sliding_window_pattern/LongestSubstringWithDistinctChars.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithDistinctChars {
+
+    public static int findLength(String str) {
+
+        int winStart = 0, maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int winEnd = 0; winEnd < str.length(); winEnd++) {
+            char rCh = str.charAt(winEnd);
+
+            if (map.containsKey(rCh)) {
+                winStart = Math.max(winStart, map.get(rCh) + 1);
+            }
+
+            map.put(rCh, winEnd);
+            maxLen = Math.max(maxLen, winEnd - winStart + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out
+                .println("Length of the longest substring: " + LongestSubstringWithDistinctChars.findLength("aabccbb"));
+        System.out.println("Length of the longest substring: " + LongestSubstringWithDistinctChars.findLength("abbbb"));
+        System.out
+                .println("Length of the longest substring: " + LongestSubstringWithDistinctChars.findLength("abccde"));
+
+    }
+}
