0e07f4ee070513ae4410455b7b1b915b22675ef1
Vlad Flore <vlad.flore@exb.de>
2022-02-03 00:04:19 +0100

    Add fast and slow pointers problems

---
+++ educative/fast_slow_pointers/LinkedListCycle.java
@@ -0,0 +1,41 @@
+package fast_slow_pointers;
+
+public class LinkedListCycle {
+
+    public static boolean hasCycle(Node head) {
+        Node fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+    }
+}
+
+class Node {
+    int data = 0;
+    Node next;
+
+    Node(int value) {
+        data = value;
+    }
+}
