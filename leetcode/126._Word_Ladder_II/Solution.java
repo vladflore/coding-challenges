a8d48eaed6264a672a6bd7cb9eb6baef20fe6638
Vlad Flore <vlad.flore@exb.de>
2022-02-07 21:00:12 +0100

    Reorganize leetcode problems

--- leetcode/126._Word_Ladder_II/Solution.java
+++ leetcode/126._Word_Ladder_II/Solution.java
@@ -1,16 +1,8 @@
-package practice.leetcode;
-
 import java.util.List;
 
 /**
  * https://leetcode.com/problems/word-ladder-ii/
  */
-public class WordLadderII {
-    public static void main(String[] args) {
-
-    }
-}
-
 class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
         return null;
