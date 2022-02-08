d8a8e94f2df1a0b1e3a0f226698ce3ca74e17bd5
Vlad Flore <vlad.flore@exb.de>
2022-02-08 12:14:49 +0100

    Add solution for top k largest numbers

---
+++ educative/grokking_coding_interview/top_k_elements/KLargestNumbers.java
@@ -0,0 +1,31 @@
+package grokking_coding_interview.top_k_elements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KLargestNumbers {
+
+    public static List<Integer> findKLargestNumbers(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.add(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(nums[i]);
+            }
+        }
+        return new ArrayList<>(minHeap);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+
+        result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
+
+}
