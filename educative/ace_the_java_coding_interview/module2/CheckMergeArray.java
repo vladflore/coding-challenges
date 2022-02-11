74ae6ff392fa7c7a9548572a44d2f87e59da49f9
Vlad Flore <vlad.flore@exb.de>
2022-02-11 01:54:08 +0100

    Solve problems

---
+++ educative/ace_the_java_coding_interview/module2/CheckMergeArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class CheckMergeArray { 
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] res = new int[len1 + len2];
+        int i = 0, j = 0, k = 0;
+
+        while(i < len1 && j < len2){
+            if(arr1[i]<arr2[j]){
+                res[k++] = arr1[i++];
+            } else {
+                res[k++] = arr2[j++];
+            }
+        }
+
+        while(i < len1){
+            res[k++] = arr1[i++];
+        }
+
+        while(j < len2){
+            res[k++] = arr2[j++];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(CheckMergeArray.mergeArrays(new int[]{1,3,4,5}, new int[]{2,6})));
+    }
+}
---
+++ educative/ace_the_java_coding_interview/module2/CheckRemoveEven.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class CheckRemoveEven {
+
+	public static int[] removeEven(int[] arr) {
+		int count = 0;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] % 2 == 0){
+				count++;
+			}
+		}
+		int[] odd = new int[arr.length- count];
+		int idx = 0;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] % 2 != 0){
+				odd[idx++] = arr[i];
+			}
+		}
+		return odd;
+	}
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(CheckRemoveEven.removeEven(new int[]{1,2,3,4,5,6,7,8,9})));
+    }
+}
