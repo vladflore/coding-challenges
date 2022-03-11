29b9c312e79573b6ef2f85f16d7954b42cd99cbe
Vlad Flore <flore.vlad@gmail.com>
2022-03-11 11:39:58 +0100

    Add new problem

---
+++ educative/grokking-coding-interview/src/main/java/tech/vladflore/educative/sliding_window/LongestSubstringWithSameLettersAfterReplacement.java
@@ -0,0 +1,12 @@
+public class LongestSubstringWithSameLetterAfterReplacement {
+
+    public static int findLength(String str, int k) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LongestSubstringWithSameLetterAfterReplacement.findLength("aabccbb", 2));
+        System.out.println(LongestSubstringWithSameLetterAfterReplacement.findLength("abbcb", 1));
+        System.out.println(LongestSubstringWithSameLetterAfterReplacement.findLength("abccde", 1));
+    }
+}
