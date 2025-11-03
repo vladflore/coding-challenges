b307413077a03324bdc4a4ca98a3724a15deaa8f
Vlad Flore <flore.vlad@gmail.com>
2025-11-04 00:11:44 +0100

    Update problem

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,11 +1,17 @@
 def contains_duplicate(numbers: list[int]) -> bool:
+    if not numbers:
+        return False
+
     seen = set()
     for number in numbers:
         if number in seen:
             return True
         seen.add(number)
+
     return False
 
-if __name__ == '__main__':
-    assert contains_duplicate([1,2,3,1])
-    assert not contains_duplicate([1,2,3,4])
+
+if __name__ == "__main__":
+    assert contains_duplicate([1, 2, 3, 1])
+    assert not contains_duplicate([1, 2, 3, 4])
+    assert not contains_duplicate([])
