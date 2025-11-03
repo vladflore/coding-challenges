0b94e4ea3d1eb015ba610da643d6bb923a14f8bf
Vlad Flore <flore.vlad@gmail.com>
2025-11-04 00:06:22 +0100

    Update scripts

--- scripts/generate_html_files.py
+++ scripts/generate_html_files.py
@@ -1,21 +1,47 @@
 import os
-
+import sys
 import markdown
 
-EXCLUDE_FILES = ["README.md"]
+EXCLUDE_MD_FILES = ["README.md"]
 
-folder_path = os.path.expanduser("~/dev/sandbox/coding-challenges/")
+FOLDER_PATH = os.path.expanduser("~/dev/sandbox/coding-challenges/")
 
-for filename in os.listdir(folder_path):
-    if filename.endswith(".md") and filename not in EXCLUDE_FILES:
-        base_name = os.path.splitext(filename)[0]
-        md_path = os.path.join(folder_path, filename)
-        html_filename = f"{base_name}.html"
-        html_path = os.path.join(folder_path, html_filename)
+md_file = sys.argv[1] if len(sys.argv) > 1 else None
 
-        with open(md_path, "r") as f:
-            md_content = f.read()
-            html = markdown.markdown(md_content)
-        with open(html_path, "w") as f:
-            f.write(html)
+
+def generate_html_file_path(md_filename):
+    base_name = os.path.splitext(md_filename)[0]
+    html_filename = f"{base_name}.html"
+    return os.path.join(FOLDER_PATH, html_filename)
+
+
+if md_file:
+    if not md_file.endswith(".md"):
+        print("Please provide a valid markdown (.md) file.")
+        sys.exit(1)
+
+    md_path = os.path.join(FOLDER_PATH, md_file)
+
+    if not os.path.isfile(md_path):
+        print(f"File {md_file} does not exist in the specified folder.")
+        sys.exit(1)
+
+    with open(md_path, "r") as f:
+        md_content = f.read()
+        html = markdown.markdown(md_content)
+
+    with open(html_filename := generate_html_file_path(md_file), "w") as f:
+        f.write(html)
         print(f"Created: {html_filename}")
+else:
+    for filename in os.listdir(FOLDER_PATH):
+        if filename.endswith(".md") and filename not in EXCLUDE_MD_FILES:
+            md_path = os.path.join(FOLDER_PATH, filename)
+
+            with open(md_path, "r") as f:
+                md_content = f.read()
+                html = markdown.markdown(md_content)
+
+            with open(html_filename := generate_html_file_path(filename), "w") as f:
+                f.write(html)
+                print(f"Created: {html_filename}")
