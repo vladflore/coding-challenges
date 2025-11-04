d6fb793ad5223c565ff8a571d55f2e7eda82fb9e
Vlad Flore <flore.vlad@gmail.com>
2025-11-04 13:48:36 +0100

    Update problem

--- is_string_palindrome.py
+++ is_string_palindrome.py
@@ -1,5 +1,5 @@
 def is_palindrome(string: str) -> bool:
-    cleaned_string = ''.join(c.lower() for c in string if c.isalnum())
+    cleaned_string = "".join(c.lower() for c in string if c.isalnum())
     left, right = 0, len(cleaned_string) - 1
     while left < right:
         if cleaned_string[left] != cleaned_string[right]:
@@ -8,11 +8,12 @@ def is_palindrome(string: str) -> bool:
         right -= 1
     return True
 
-if __name__ == '__main__':
-    import sys
-    args = sys.argv
-    if len(args) < 2:
-        raise ValuesError('usage: python is_string_palindrome.py <your_string>')
-    string = args[1]
-    print(f'{string} {"is" if is_palindrome(string) else "is not"} a palindrome')
 
+if __name__ == "__main__":
+    assert is_palindrome("A man, a plan, a canal: Panama")
+    assert not is_palindrome("race a car")
+    assert is_palindrome("")
+    assert is_palindrome("No 'x' in Nixon")
+    assert not is_palindrome("Hello, World!")
+    assert is_palindrome("12321")
+    assert not is_palindrome("12345")
