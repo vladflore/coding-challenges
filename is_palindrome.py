8edec6a3383a2124192f60f090c854742ad7cef6
Vlad Flore <flore.vlad@gmail.com>
2025-11-04 14:20:17 +0100

    Update problem

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,24 +1,21 @@
 def is_palindrome(num: int) -> bool:
-
-    if num < 0 or num % 10 == 0 and num != 0:
+    if num < 0 or (num % 10 == 0 and num != 0):
         return False
 
-    reversed = 0
-    while num > reversed:
-        reversed = reversed * 10 + num % 10
+    reversed_half = 0
+    while num > reversed_half:
+        last_digit = num % 10
+        reversed_half = reversed_half * 10 + last_digit
         num //= 10
 
-    if num == reversed or num == reversed // 10:
-        return True
+    return num == reversed_half or num == reversed_half // 10
 
-    return False
 
-if __name__ == '__main__':
-    print(f'1 -> {is_palindrome(1)}')
-    print(f'12 -> {is_palindrome(12)}')
-    print(f'121 -> {is_palindrome(121)}')
-    print(f'12321 -> {is_palindrome(12321)}')
-    print(f'1221 -> {is_palindrome(1221)}')
-    print(f'120 -> {is_palindrome(120)}')
-    print(f'-1 -> {is_palindrome(-1)}')
-
+if __name__ == "__main__":
+    assert is_palindrome(121)
+    assert not is_palindrome(-121)
+    assert not is_palindrome(10)
+    assert is_palindrome(12321)
+    assert is_palindrome(0)
+    assert not is_palindrome(1234)
+    assert is_palindrome(123321)
