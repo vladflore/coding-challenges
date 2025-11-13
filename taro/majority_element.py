3937dc8e5cb0bcd0c4db3aa28f78a72d13adaf10
Vlad Flore <flore.vlad@gmail.com>
2025-11-13 12:28:25 +0100

    Remove taro files

--- taro/majority_element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-        counter = Counter(nums)
-        for num, count in counter.items():
-            if count > len(nums) // 2:
-                return num
-                
